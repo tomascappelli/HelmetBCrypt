b8a49250e9f51a34f12f0cd1be580fdad5dcb6dc
tomascappelli <tomascappelli@hotmail.com>
2023-05-18 11:50:09 -0300

    bcrypt.hash

--- server.js
+++ server.js
@@ -11,7 +11,6 @@ const someOtherPlaintextPassword = 'pass123';
 const helmet = require('helmet');
 const bcrypt = require('bcrypt');
 
-
 app.use(helmet({
   frameguard: {         
     action: 'deny'
@@ -28,7 +27,13 @@ app.use(helmet({
 
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
 
-
+bcrypt.hash('passw0rd!', 13, (err, hash) => {
+  console.log(hash);
+  //$2a$12$Y.PHPE15wR25qrrtgGkiYe2sXo98cjuMCG1YwSI5rJW1DSJp0gEYS
+  bcrypt.compare('passw0rd!', hash, (err, res) => {
+    console.log(res); //true
+  });
+});
 
 //END_ASYNC
 
