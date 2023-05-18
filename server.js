6571808b1550133b202bf33e1f5ce1bfef7b0379
tomascappelli <tomascappelli@hotmail.com>
2023-05-18 11:56:47 -0300

    bcrypt.hash

--- server.js
+++ server.js
@@ -30,10 +30,10 @@ bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
 
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
 
-bcrypt.hash(myPlaintextPassword, 13, (err, hash) => {
+bcrypt.hash('passw0rd!', 13, (err, hash) => {
   console.log(hash);
   //$2a$12$Y.PHPE15wR25qrrtgGkiYe2sXo98cjuMCG1YwSI5rJW1DSJp0gEYS
-  bcrypt.compare(myPlaintextPassword, hash, (err, res) => {
+  bcrypt.compare('passw0rd!', hash, (err, res) => {
     console.log(res); //true
   });
 });
