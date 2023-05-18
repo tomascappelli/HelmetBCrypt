09c66a707e3ba81deda0ddec2924eaf1f5dccc8b
tomascappelli <tomascappelli@hotmail.com>
2023-05-18 11:59:57 -0300

    bcrypt.hash

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const bodyParser  = require('body-parser');
 const fccTesting  = require('./freeCodeCamp/fcctesting.js');
 const app         = express();
 fccTesting(app);
-const saltRounds = 12;
+const saltRounds = 13;
 const myPlaintextPassword = 'sUperpassw0rd!';
 const someOtherPlaintextPassword = 'pass123';
 
@@ -30,10 +30,10 @@ bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
 
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
 
-bcrypt.hash('passw0rd!', 13, (err, hash) => {
+bcrypt.hash('sUperpassw0rd!', 13, (err, hash) => {
   console.log(hash);
   //$2a$12$Y.PHPE15wR25qrrtgGkiYe2sXo98cjuMCG1YwSI5rJW1DSJp0gEYS
-  bcrypt.compare('passw0rd!', hash, (err, res) => {
+  bcrypt.compare('sUperpassw0rd!', hash, (err, res) => {
     console.log(res); //true
   });
 });
