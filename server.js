282ce57c5d2acf9ad24212540a3b0fd22fefda3a
tomascappelli <tomascappelli@hotmail.com>
2023-05-18 11:55:09 -0300

    bcrypt.hash

--- server.js
+++ server.js
@@ -27,10 +27,10 @@ app.use(helmet({
 
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
 
-bcrypt.hash('passw0rd!', 13, (err, hash) => {
+bcrypt.hash(myPlaintextPassword, 13, (err, hash) => {
   console.log(hash);
   //$2a$12$Y.PHPE15wR25qrrtgGkiYe2sXo98cjuMCG1YwSI5rJW1DSJp0gEYS
-  bcrypt.compare('passw0rd!', hash, (err, res) => {
+  bcrypt.compare(myPlaintextPassword, hash, (err, res) => {
     console.log(res); //true
   });
 });
